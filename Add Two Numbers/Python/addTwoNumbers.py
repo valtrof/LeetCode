fbde342172170ed316abcb50018608b2832505bc
valtrof <99944750+valtrof@users.noreply.github.com>
2022-02-18 01:29:23 -0800

    improve formatting

--- Add Two Numbers/Python/addTwoNumbers.py
+++ Add Two Numbers/Python/addTwoNumbers.py
@@ -9,7 +9,7 @@ class ListNode:
 
 class Solution:
     # 95.31, 82.16, first attempt, recursive
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode], carry: int=0) -> Optional[ListNode]:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode], carry: int = 0) -> Optional[ListNode]:
         if l1 is None and l2 is None: return ListNode(1) if carry == 1 else None
         sum = (0 if l1 is None else l1.val) + (0 if l2 is None else l2.val) + carry
         return ListNode(sum % 10, self.addTwoNumbers(None if l1 is None else l1.next, None if l2 is None else l2.next, sum // 10))
