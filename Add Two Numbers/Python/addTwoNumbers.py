90e91a980213e7ecb206351d7cc003f18d2de8c3
valtrof <99944750+valtrof@users.noreply.github.com>
2022-02-18 00:43:15 -0800

    simplify if statement

--- Add Two Numbers/Python/addTwoNumbers.py
+++ Add Two Numbers/Python/addTwoNumbers.py
@@ -11,9 +11,7 @@ class Solution:
     # 79.65, 82.16, first attempt, recursive
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         def nextNode(l1: Optional[ListNode], l2: Optional[ListNode], carry: int) -> Optional[ListNode]:
-            if l1 is None and l2 is None:
-                if carry == 1: return ListNode(1)
-                else: return None
+            if l1 is None and l2 is None: return ListNode(1) if carry == 1 else None
             sum = (0 if l1 is None else l1.val) + (0 if l2 is None else l2.val) + carry
             return ListNode(sum % 10, nextNode(None if l1 is None else l1.next, None if l2 is None else l2.next, sum // 10))
         return nextNode(l1, l2, 0)
