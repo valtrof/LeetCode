3201554e62dd43232f7dccfa8b27bee3592a3634
valtrof <99944750+valtrof@users.noreply.github.com>
2022-02-18 01:20:59 -0800

    simplify main code

--- Add Two Numbers/Python/addTwoNumbers.py
+++ Add Two Numbers/Python/addTwoNumbers.py
@@ -8,21 +8,20 @@ class ListNode:
 
 
 class Solution:
-    # 79.65, 82.16, first attempt, recursive
+    # 95.31, 82.16, first attempt, recursive
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode], carry: int=0) -> Optional[ListNode]:
         if l1 is None and l2 is None: return ListNode(1) if carry == 1 else None
         sum = (0 if l1 is None else l1.val) + (0 if l2 is None else l2.val) + carry
         return ListNode(sum % 10, self.addTwoNumbers(None if l1 is None else l1.next, None if l2 is None else l2.next, sum // 10))
 
 
-s = Solution()
+if __name__ == '__main__':
+    a, a.next, a.next.next = ListNode(2), ListNode(4), ListNode(3)
+    b, b.next, b.next.next = ListNode(5), ListNode(6), ListNode(4)
+    res = Solution().addTwoNumbers(a, b)
+    print(f"[{res.val},{res.next.val},{res.next.next.val}]")
 
-res = s.addTwoNumbers(
-    ListNode(2, ListNode(4, ListNode(3))),
-    ListNode(5, ListNode(6, ListNode(4))))
-print(",".join([str(x) for x in [res.val, res.next.val, res.next.next.val]]))
-
-res = s.addTwoNumbers(
-    ListNode(9, ListNode(9)),
-    ListNode(9))
-print(",".join([str(x) for x in [res.val, res.next.val, res.next.next.val]]))
+    a, a.next = ListNode(9), ListNode(9)
+    b = ListNode(9)
+    res = Solution().addTwoNumbers(a, b)
+    print(f"[{res.val},{res.next.val},{res.next.next.val}]")
