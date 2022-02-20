ac33909c701bda4f0c450bebcd82b626acea15f2
valtrof <99944750+valtrof@users.noreply.github.com>
2022-02-20 01:20:46 -0800

    add Search Insert Position

---
+++ 35. Search Insert Position/CSharp/Search Insert Position/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        var l = 0;
+        var r = nums.Length - 1;
+        int mid;
+
+        while (l <= r)
+        {
+            mid = (l + r) / 2;
+            if (target == nums[mid]) return mid;
+            if (target < nums[mid]) r = mid - 1;
+            else l = mid + 1;
+        }
+
+        return l;
+    }
+}
