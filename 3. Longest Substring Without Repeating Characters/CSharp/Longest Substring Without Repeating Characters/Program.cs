204d5bcda019bf5bca7a707677009aa61d35b1ca
valtrof <99944750+valtrof@users.noreply.github.com>
2022-02-20 04:22:07 -0800

    add Longest Substring Without Repeating Characters

---
+++ 3. Longest Substring Without Repeating Characters/CSharp/Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,23 @@
+﻿var s = new Solution();
+Console.WriteLine(s.LengthOfLongestSubstring("dvdf"));
+
+Console.ReadKey();
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        var set = new HashSet<int>();
+        int l = 0, r = 0, max = 0;
+
+        while (r < s.Length)
+        {
+            while (set.Contains(s[r])) set.Remove(s[l++]);
+            set.Add(s[r++]);
+            max = Math.Max(max, r - l);
+        }
+
+        return max;
+    }
+}
